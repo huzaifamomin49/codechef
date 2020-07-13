1b515a691862dfa043bb49f33fcdf2c5b564cbf0
Huzaifa Momin <47695127+huzaifamomin49@users.noreply.github.com>
2020-07-13 20:52:05 +0530

    Problem Code : TEST

---
+++ DSA Learning Series/Complexity analysis + Basic Warm up/Life-the-Universe-and-Everything.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	while(1){
+	    int b;
+	    cin>>b;
+	    if(b==42)
+	        break;
+	   cout<<b<<endl;
+	   
+	}
+	return 0;
+}
